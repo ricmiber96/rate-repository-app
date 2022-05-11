938ef7acf16d862fbab5344ff43e552d6d2fbb2e
Ricardo Miralles <ricmiber@gmail.com>
2022-05-11 11:36:14 +0200

    dded custom Hook and Graphql client

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient'
 
+
+
+const apolloClient = createApolloClient()
 
 export default function App() {
   return (
     <>
-    <StatusBar style="light"/>
+    <ApolloProvider client={apolloClient}>
+      <StatusBar style="light"/>
       <NativeRouter>
         <Main/>
       </NativeRouter>
+    </ApolloProvider>
     </>
   );
 }
